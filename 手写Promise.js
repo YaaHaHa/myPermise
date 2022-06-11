89f5dc84cc8c1ebda87c2ad6b3445b26d0e55676
YaaHaHa <1902793546@qq.com>
2022-06-12 00:08:35 +0800

    修改then返回新的myPromise失败的bug

--- 手写Promise.js
+++ 手写Promise.js
@@ -6,6 +6,24 @@
     Promise的状态改成rejected后有一个唯一的reason
 
 */
+
+// 遍历执行实例上由then产生的由状态决定是否执行的回调函数
+function fulfilledCall(fulfilledList) {
+    if (fulfilledList.length) {
+        fulfilledList.forEach((fn) => {
+            fn()
+        })
+    }
+}
+
+function rejectedCall(rejectedList) {
+    if (rejectedList.length) {
+        rejectedList.forEach((fn) => {
+            fn();
+        })
+    }
+}
+
 function myPromise(executor) {
     this.state = 'pending';
     this.value = null;
@@ -23,22 +41,19 @@ function myPromise(executor) {
             // console.log('使用了resolve');
             if (value instanceof myPromise) {
                 // 能进入这里，说明当前的this是then将要返回的Promise，value是then回调里面返回的promise
-                // 这里和then有关，因为then在处理then里面的fulfilledCall和rejectedCall时都要执行这里，捕获一下错误
+                // ！！！！！！！！！！！！！！这里和then有关，因为then在处理then里面的fulfilledCall和rejectedCall时都要执行这里，捕获一下错误
                 if (value.state === 'fulfilled') {
                     this.state = 'fulfilled';
                     this.value = value.value;
                 } else if (value.state === 'rejected') {
                     this.state = 'rejected';
                     this.reason = value.reason;
-                    throw new Error(this.reason);
+                    rejectedCall(this.rejectedList);
+                    throw new Error(this.reason);   // 这里抛出错误了，后面就执行不了了，所以导致返回rejected的promise的话到这里就断了，而且在then那边直接捕获错误抛给reject，但是此时state已经是rejected了，所以即将进入的reject根本进不去if，后面的回调无法执行
                 }
             }
             // 当状态由padding转成fuifilled时才会执行下面的语句
-            if (this.fulfilledList.length) {
-                this.fulfilledList.forEach((fn) => {
-                    fn()
-                })
-            }
+            fulfilledCall(this.fulfilledList)
         }
     }
 
@@ -47,9 +62,7 @@ function myPromise(executor) {
             // console.log('使用了reject');
             this.state = 'rejected';
             this.reason = reason;
-            this.rejectedList.forEach((fn) => {
-                fn();
-            })
+            rejectedCall(this.rejectedList);
         }
     }
     try {
@@ -64,15 +77,18 @@ function myPromise(executor) {
 
 
 myPromise.prototype.then = function (onFufilledCall, onRejectCedall) {
-    // 如果then的成功回调不是函数类型的，直接返回value
-    if (typeof onFufilledCall !== 'function') {
+    // 如果then的成功回调不是函数类型的，直接返回value，
+    // ！！！！！！！！！第一次执行fulfilledList是空的，不能让undefined进入下面的id
+    if (typeof onFufilledCall !== 'function' && onFufilledCall !== undefined) {
         onFufilledCall = (value) => {
             return value;
         }
     }
 
     // 如果then的失败回调不是函数类型的，throw error
-    if (typeof onRejectCedall !== 'function') {
+    if (typeof onRejectCedall !== 'function' && onRejectCedall !== undefined) {
+        console.log(this);
+        console.log(onRejectCedall);
         onRejectCedall = (reason) => {
             throw new Error(reason);
         }
@@ -85,12 +101,15 @@ myPromise.prototype.then = function (onFufilledCall, onRejectCedall) {
             this.fulfilledList.push(() => {
                 // this与函数定义的地方无关，与使用的地方有关，这个onfilledCall是在resolve后执行的，当前实例肯定有value啊
                 const x = onFufilledCall(this.value);
-                try {
-                    // 因为.then的返回值有多种情况，当x是error或rejected状态的promise时被catch到
+                if (x === undefined){
                     resolve(x)
-                } catch (reason) {
-                    reject(reason)
                 }
+                    try {
+                        // 因为.then的返回值有多种情况，当x是error或rejected状态的promise时被catch到
+                        resolve(x)
+                    } catch (reason) {
+                        reject(reason)
+                    }
 
             })
             // 把
@@ -199,12 +218,26 @@ const p1 = new myPromise((resolve, reject) => {
         resolve('success_p1');
     }, 3000)
 });
-p1.test = '我是p1'
+p1.name = 'p1';
+// p1.test = '我是p1'
 const p2 = p1.then(() => {
     // console.log('then里成功的回调');
-    return myPromise.reject('p2的reason')
-    // return 2;
+    // 定时器构造的异步函数中return是不行的，return给谁啊，没人接啊。误以为是onFufilledCall的返回值
+    return myPromise.reject('p2的reason');
 })
+p2.name = 'p2'
+
+// 蚌埠住了，p2是rejected，那p2的then里面要写第二个函数处理rejected啊，不然p3怎么办！！！
+/* const p3 = p2.then(() => {
+    console.log('我是p3');
+    return myPromise.reject('p3的reason');
+}, () => {
+    console.log('我是p3');
+    return myPromise.reject('p3的reason');
+}
+)
+p3.name = 'p3' */
+
 
 
 const p3 = new myPromise((resolve, reject) => {
@@ -213,13 +246,19 @@ const p3 = new myPromise((resolve, reject) => {
         resolve('success_p3');
     }, 1000)
 });
+p3.name = 'p3';
 
 const p4 = new myPromise((resolve, reject) => {
-    // console.log('loading');
     setTimeout(() => {
         reject('fail');
     }, 5000)
 });
+p4.name = 'p4'
 
 const pAll = myPromise.all([p1, p2, p3, p4]);
+pAll.name = 'pAll';
+
+// BUG:p2比p4块，最后pAll却是p4?
+// 在solve那里添加instenceof判断，因为then里面会把onFufilledCall的返回值(成功回调的返回值)拿去再给resolve处理，同时try..catch捕获，可以这样把reject抛出来
+// p1的then里面再setTimeout里面return myPrmise.reject()，这里的return根本不是onFufilledCall的返回值
 
